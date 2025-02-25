eb2d4d9137ffb49772c34f4354852ec165e23406
dev-gaitano <dev.gaitano@gmail.com>
2025-02-25 16:59:12 +0300

    Build commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
-  base: "/#collections",
+  base: "/nato-fashion-house/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
